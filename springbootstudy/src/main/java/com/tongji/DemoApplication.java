e6af43df6cdd945db5c43d14dd4100b1fa2ecff5
Mr-zhang999 <1041872694@qq.com>
2021-09-12 20:54:29 +0800

    fix

--- springbootstudy/src/main/java/com/tongji/DemoApplication.java
+++ springbootstudy/src/main/java/com/tongji/DemoApplication.java
@@ -11,8 +11,8 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 //@MapperScan("com/tongji/demo/mapper")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
