45e82d49079a5c7fdfabd1596c1d6bcf68e1611e
Mr-zhang999 <1041872694@qq.com>
2021-11-26 17:35:44 +0800

    fix

--- springbootstudy/src/test/java/com/tongji/rabbitmq/workqueue/Producer.java
+++ springbootstudy/src/test/java/com/tongji/rabbitmq/workqueue/Producer.java
@@ -7,6 +7,7 @@ import com.tongji.rabbitmq.rabbitutils.RabbitMqUtil;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.util.UUID;
 import java.util.concurrent.TimeoutException;
 
 /*
@@ -14,20 +15,39 @@ import java.util.concurrent.TimeoutException;
  * @date 2021-11-23 11:01
  */
 public class Producer {
-
     private static final String QUEUE_NAME = "HELLO";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         Channel channel = RabbitMqUtil.getChannel();
-
-
         channel.queueDeclare(QUEUE_NAME, true, false, false, null);
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String message = sc.nextLine();
-            channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes(StandardCharsets.UTF_8));
             System.out.println("message send over:" + message);
         }
+    }
 
+    public static void publishMessageIndividually() throws Exception {
+        try (Channel channel = RabbitMqUtil.getChannel()) {
+            String queueName = UUID.randomUUID().toString();
+            channel.queueDeclare(queueName, false, false, false, null);
+            //开启发布确认
+            channel.confirmSelect();
+            long begin = System.currentTimeMillis();
+            for (int i = 0; i < 10; i++) {
+                String message = i + "";
+                channel.basicPublish("", queueName, null, message.getBytes());
+                //服务端返回 false 或超时时间内未返回，生产者可以消息重发
+                boolean flag = channel.waitForConfirms();
+                if (flag) {
+                    System.out.println("消息发送成功");
+                }
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("发布" + 10 + "个单独确认消息,耗时" + (end - begin) +
+                    "ms");
+        }
     }
 }
